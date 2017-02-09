c0196d0dcf945d480122dc44fdb5a719c52d3ff0
Afranio Martins <afranio.martins@fariasbrito.com.br>
2017-02-09 16:48:39 -0300

    correção comando

--- deployer/rsync/rsync-entrypoint.sh
+++ deployer/rsync/rsync-entrypoint.sh
@@ -5,5 +5,3 @@ if [ "$1" = 'deployer' ]; then
     ssh-add <(echo "$DEPLOY_SSH_PUBLIC_KEY_BASE64" | base64 -d);
     rsync -avuzpg --delete --delete-excluded --filter='dir-merge .rsync-filter' $DEPLOY_PROJECT_DIR/. $DEPLOY_CONNECTION/$DEPLOY_PROJECT_PATH;
 fi
-
-exec "$@"
