02c7a2e36432fdd0762da242ff208d3419c80c23
Afranio Martins <afranio.martins@fariasbrito.com.br>
2017-02-10 13:48:28 -0300

    alterando comando

--- deployer/rsync/deploy.sh
+++ deployer/rsync/deploy.sh
@@ -2,6 +2,7 @@
 set -e
 
 if [ "$1" == 'exec' ]; then
+    eval $(ssh-agent -s);
     ssh-add <(echo "$DEPLOY_SSH_PUBLIC_KEY_BASE64" | base64 -d);
     rsync -avuzpg --delete --delete-excluded --filter='dir-merge .rsync-filter' $DEPLOY_PROJECT_DIR/. $DEPLOY_CONNECTION/$DEPLOY_PROJECT_PATH;
 fi
