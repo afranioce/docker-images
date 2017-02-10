2ff792ed5d5970034d84008d4508a156773f7290
Afranio Martins <afranioce@gmail.com>
2017-02-09 22:52:18 -0300

    correção de versão

--- deployer/lftp/lftp-entrypoint.sh
+++ deployer/lftp/lftp-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'deployer' ]; then
+if [ "$1" == 'deployer' ]; then
   lftp -u $DEPLOY_USER,$DEPLOY_PASSWORD -e "set ssl:verify-certificate no; mirror -n -R -vvv --exclude-glob-from '.syncignore' $DEPLOY_PROJECT_DIR/. $DEPLOY_PUBLIC_DIR/$DEPLOY_PROJECT_PATH" $DEPLOY_HOST;
 fi
+
+exec "$@"
--- deployer/rsync/rsync-entrypoint.sh
+++ deployer/rsync/rsync-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'deployer' ]; then
+if [ "$1" == 'deployer' ]; then
     ssh-add <(echo "$DEPLOY_SSH_PUBLIC_KEY_BASE64" | base64 -d);
     rsync -avuzpg --delete --delete-excluded --filter='dir-merge .rsync-filter' $DEPLOY_PROJECT_DIR/. $DEPLOY_CONNECTION/$DEPLOY_PROJECT_PATH;
 fi
+
+exec "$@"
