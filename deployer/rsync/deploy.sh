e93db0d9bab7a73eeb0c07305bf1fece75b1c0bf
Afranio Martins <afranioce@users.noreply.github.com>
2018-07-03 15:21:12 -0300

    fix: preserve files to exlude

--- deployer/rsync/deploy.sh
+++ deployer/rsync/deploy.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" == 'exec' ]; then
     eval $(ssh-agent -s);
     ssh-add <(echo "$DEPLOY_PK_BASE64" | base64 -d);
-    rsync -avuzpg --delete --delete-excluded --filter='dir-merge .rsync-filter' $DEPLOY_PROJECT_DIR/. $DEPLOY_CONNECTION/$DEPLOY_PROJECT_PATH;
+    rsync -avzpgCDF --delete-after $DEPLOY_PROJECT_DIR/. $DEPLOY_CONNECTION/$DEPLOY_PROJECT_PATH
 fi
 
 exec "$@"
